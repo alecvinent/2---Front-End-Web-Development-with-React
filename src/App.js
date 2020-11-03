107be93fffa85bdcad62afae9b5ba60f5d6fc4fd
alec <aleck.vinent@gmail.com>
2020-11-03 17:02:15 -0500

    React Components Part 2

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import MenuComponent from './components/MenuComponent';
+import { DISHES } from "./shared/dishes";
 import './App.css';
 
 const logo = './assets/images/logo.png';
+const title = 'Ristorante Con Fusion';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
   render() {
 
     return (
@@ -14,11 +25,11 @@ class App extends Component {
         <Navbar dark color="primary" expand="sm" fixed="top">
           <div className="container">
             <NavbarBrand href="/">
-              <img src={logo} alt="Ristorante Con Fusion" height="30" width="41" />
+              <img src={logo} alt={title} height="30" width="41" />
             </NavbarBrand>
           </div>
         </Navbar>
-        <MenuComponent />
+        <MenuComponent dishes={this.state.dishes} />
       </div>
     );
   }
