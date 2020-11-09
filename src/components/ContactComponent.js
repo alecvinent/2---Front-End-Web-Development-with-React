52f39ca9333675d612947b93ceb5a6d7c1b53474
alec <aleck.vinent@gmail.com>
2020-11-08 19:56:49 -0500

    Form validation

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
 import { Breadcrumb, BreadcrumbItem, Col } from "reactstrap";
-import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText,
+  FormFeedback,
+} from "reactstrap";
 import { Link } from "react-router-dom";
 
 class Contact extends Component {
@@ -16,32 +24,101 @@ class Contact extends Component {
       agree: false,
       contactType: "telephone",
       message: "",
+      touched: {
+        firstname: false,
+        lastname: false,
+        telnum: false,
+        email: false,
+      },
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
   }
 
   //
-  handleInputChange(event){
+  handleInputChange(event) {
     const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const value = target.type === "checkbox" ? target.checked : target.value;
     const name = target.name;
 
     this.setState({
-      [name] : value
+      [name]: value,
     });
-  };
-  
+  }
+
   //
-  handleSubmit(event){
-    console.log('current state is:' + JSON.stringify(this.state));
-    alert('current state is:' + JSON.stringify(this.state));
-    event.preventDefault();
+  handleBlur = (field) => (evt) => {
+    this.setState({
+      touched: { ...this.state.touched, [field]: true },
+    });
   };
 
   //
+  validateFields(firstname, lastname, telnum, email) {
+    const errors = {
+      firstname: "",
+      lastname: "",
+      telnum: "",
+      email: "",
+    };
+
+    // firstname
+    if (this.state.touched.firstname && firstname.length < 3) {
+      errors.firstname = "First name should be greather than 3 characters";
+    } else if (this.state.touched.firstname && firstname.length > 10) {
+      errors.firstname = "First name should be less than 10 characters";
+    }
+
+    // lastname
+    if (this.state.touched.lastname && lastname.length < 3) {
+      errors.firstname = "Last name should be greather than 3 characters";
+    } else if (this.state.touched.lastname && lastname.length < 10) {
+      errors.lastname = "Last name should be greather than 10 characters";
+    }
+
+    // telnum. Pe.: +5353793780, +53 53793780, +53-53793780
+    // const telnum_reg = new RegExp(
+    //   "/(?<country_code>+[1-9]{2}?)(?<separator>[()s.-]?)((?<number>.*))/"
+    // );
+
+    // const telnum_reg = /^[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-/\s.]?[0-9]{4}$/;
+
+    // const telnum_reg = /^\d+$/;
+    const telnum_reg = /((\+)?[1-9]{2})(.*)/;
+    if (this.state.touched.telnum && !telnum_reg.test(telnum)) {
+      errors.telnum = "Wrong format";
+    }
+
+    // email
+    // const email_reg = new RegExp("^((?!.)[w-_.]*[^.])(@w+)(.w+(.w+)?[^.W])$");
+    // const email_reg = new RegExp("/\S+@\S+\.\S+/");
+    if (
+      this.state.touched.email &&
+      email.split("").filter((x) => x === "@").length !== 1
+    ) {
+      errors.email = "Wrong format";
+    }
+
+    return errors;
+  }
+
+  //
+  handleSubmit(event) {
+    // console.log("current state is:" + JSON.stringify(this.state));
+    event.preventDefault();
+  }
+
+  //
   render() {
+    const errors = this.validateFields(
+      this.state.firstname,
+      this.state.lastname,
+      this.state.telnum,
+      this.state.email
+    );
+
     return (
       <div className="container">
         <div className="row">
@@ -127,10 +204,16 @@ class Contact extends Component {
                     className="form-control"
                     placeholder="First Name"
                     value={this.state.firstname}
+                    valid={errors.firstname === ""}
+                    invalid={errors.firstname !== ""}
                     aria-describedby="firstname-help"
                     onChange={this.handleInputChange}
+                    onBlur={this.handleBlur("firstname")}
                   />
-                  <FormText color="muted" id="firstname-help">Help text</FormText>
+                  <FormText color="muted" id="firstname-help">
+                    Help text
+                  </FormText>
+                  <FormFeedback>{errors.firstname}</FormFeedback>
                 </Col>
               </FormGroup>
 
@@ -146,10 +229,16 @@ class Contact extends Component {
                     className="form-control"
                     placeholder="Last Name"
                     value={this.state.lastname}
+                    valid={errors.lastname === ""}
+                    invalid={errors.lastname !== ""}
                     aria-describedby="lastname-help"
                     onChange={this.handleInputChange}
+                    onBlur={this.handleBlur("lastname")}
                   />
-                  <FormText color="muted" id="lastname-help">Help text</FormText>
+                  <FormText color="muted" id="lastname-help">
+                    Help text
+                  </FormText>
+                  <FormFeedback>{errors.lastname}</FormFeedback>
                 </Col>
               </FormGroup>
 
@@ -165,16 +254,22 @@ class Contact extends Component {
                     className="form-control"
                     placeholder="Tel. number"
                     value={this.state.telnum}
+                    valid={errors.telnum === ""}
+                    invalid={errors.telnum !== ""}
                     aria-describedby="telnum-help"
                     onChange={this.handleInputChange}
+                    onBlur={this.handleBlur("telnum")}
                   />
-                  <FormText color="muted" id="telnum-help">Help text</FormText>
+                  <FormText color="muted" id="telnum-help">
+                    Format: +5353793780, +53 53793780, +53-53793780
+                  </FormText>
+                  <FormFeedback>{errors.telnum}</FormFeedback>
                 </Col>
               </FormGroup>
 
               <FormGroup row>
                 <Label htmlFor="email" md={2} className="col-form-label">
-                  Contact Tel.
+                  Email
                 </Label>
                 <Col md={10}>
                   <Input
@@ -184,10 +279,16 @@ class Contact extends Component {
                     className="form-control"
                     placeholder="Email"
                     value={this.state.email}
+                    valid={errors.email === ""}
+                    invalid={errors.email !== ""}
                     aria-describedby="email-help"
                     onChange={this.handleInputChange}
+                    onBlur={this.handleBlur("email")}
                   />
-                  <FormText color="muted" id="email-help">Help text</FormText>
+                  <FormText color="muted" id="email-help">
+                    Help text
+                  </FormText>
+                  <FormFeedback>{errors.email}</FormFeedback>
                 </Col>
               </FormGroup>
 
@@ -237,16 +338,19 @@ class Contact extends Component {
                     aria-describedby="message-help"
                     onChange={this.handleInputChange}
                   />
-                  <FormText color="muted" id="message-help">Help text</FormText>
+                  <FormText color="muted" id="message-help">
+                    Help text
+                  </FormText>
                 </Col>
               </FormGroup>
 
               <FormGroup row>
-              <Col md={{ offset: 2, size: 10 }}>
-                  <Button type="submit" color="primary">Send Feedback</Button>
+                <Col md={{ offset: 2, size: 10 }}>
+                  <Button type="submit" color="primary">
+                    Send Feedback
+                  </Button>
                 </Col>
               </FormGroup>
-
             </Form>
           </div>
         </div>
