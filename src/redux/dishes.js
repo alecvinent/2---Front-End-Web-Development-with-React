7eeb7ae71614ffdc4eba732d5fd62ac63184428a
alec <aleck.vinent@gmail.com>
2020-11-11 22:11:44 -0500

    mergin reducers

---
+++ src/redux/dishes.js
@@ -0,0 +1,10 @@
+// shared
+import { DISHES } from "../shared/dishes";
+
+//
+export const Dishes = (state = DISHES, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
